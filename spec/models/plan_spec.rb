e4a277998de6c85207406ae7ca80eb50acd65f0d
Duane M. Moody <dmmoody@gmail.com>
2015-08-25 09:09:25 -0700

    add paypal payments

---
+++ spec/models/plan_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+describe Plan do 
+  it { should have_many :subscriptions }
+  it { should validate_presence_of :name }
+  it { should validate_presence_of :price }
+  it { should validate_presence_of :paypal_description }
+  it { should validate_presence_of :description }
+end
---
+++ spec/models/subscription_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+describe Subscription do 
+  it { should belong_to :plan }
+  it { should validate_presence_of :paypal_payer_id }
+  it { should validate_presence_of :paypal_profile_id }
+  it { should validate_presence_of :paid_until }
+  it { should validate_presence_of :canceled }
+end
