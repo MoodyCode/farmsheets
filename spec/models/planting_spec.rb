a506a67410489adea9b48d870d10b8080116ecf4
Duane M. Moody <dmmoody@gmail.com>
2015-08-25 11:02:08 -0700

    add user_id column to plantings and add associations and specs

--- spec/models/planting_spec.rb
+++ spec/models/planting_spec.rb
@@ -4,6 +4,7 @@ describe Planting do
   it { should have_one :harvest }
   it { should belong_to :quantity }
   it { should belong_to :size }
+  it { should belong_to :user }
 
   it { should validate_presence_of :crop }
   it { should validate_presence_of :variety }
