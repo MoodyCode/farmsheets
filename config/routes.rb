7b54663319a0c355d9d35e9dd4b68c3f319315d5
Duane M. Moody <dmmoody@gmail.com>
2015-08-25 12:31:13 -0700

    add small corrections

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   authenticated :user do
-    root to: "plantings#show", as: :authenticated_root
+    root to: "plantings#index", as: :authenticated_root
   end
 
   root to: "static_pages#index"
