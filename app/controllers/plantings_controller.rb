85e862b7fb0459a18f0552ee7c0c2c3d7f656bc2
Duane M. Moody <dmmoody@gmail.com>
2015-08-29 14:34:07 -0700

    add plantings form partial layouts

--- app/controllers/plantings_controller.rb
+++ app/controllers/plantings_controller.rb
@@ -4,11 +4,19 @@ class PlantingsController < ApplicationController
   def index
     @user = current_user
     @plantings = @user.plantings.all
+    @planting = Planting.new
+    @crops = Crop.all
+    @varietals = Varietal.all
+    @harvest = Harvest.new
   end
 
   def show
     @user = current_user
     @plantings = @user.plantings.all
+  end
+
+  def new
+    @planting = Planting.new
   end
 
 end
