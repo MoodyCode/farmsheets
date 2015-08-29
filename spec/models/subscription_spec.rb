a1de24852e0819a49bd34810bb462e65a15720c4
Duane M. Moody <dmmoody@gmail.com>
2015-08-29 14:53:32 -0700

    corrects plantings form layout

--- spec/models/subscription_spec.rb
+++ spec/models/subscription_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 describe Subscription do 
   it { should belong_to :plan }
+  it { should belong_to :user }
   it { should validate_presence_of :paypal_payer_id }
   it { should validate_presence_of :paypal_profile_id }
   it { should validate_presence_of :paid_until }
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,4 +2,5 @@ require 'rails_helper'
 
 describe User do
   it { should have_many :plantings }
+  it { should have_one :subscription }
 end
