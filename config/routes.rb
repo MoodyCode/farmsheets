340660d9cb597de8e9ca11ae1aa4701956ec2d1b
Duane M. Moody <dmmoody@gmail.com>
2015-08-29 12:16:39 -0700

    add subscription options

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,18 @@
 Rails.application.routes.draw do
   devise_for :users
 
+  # resources :subscriptions do
+  #   member do
+  #     get :make_recurring
+  #   end
+  # end
+
+  # resources :plans
+
+  # post 'paypal/ipn_listener' => 'paypal#ipn_listener'
+
+  # root 'plans#index'
+
   authenticated :user do
     root to: "plantings#index", as: :authenticated_root
   end
--- spec/models/plan_spec.rb
+++ spec/models/plan_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 describe Plan do 
   it { should have_many :subscriptions }
-  # it { should validate_presence_of :name }
-  # it { should validate_presence_of :price }
-  # it { should validate_presence_of :paypal_description }
-  # it { should validate_presence_of :description }
+  it { should validate_presence_of :name }
+  it { should validate_presence_of :price }
+  it { should validate_presence_of :paypal_description }
+  it { should validate_presence_of :description }
 end
