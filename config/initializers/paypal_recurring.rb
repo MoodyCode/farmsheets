3d0c22aba2d3926f36fa96b6776811ce0605485f
Duane M. Moody <dmmoody@gmail.com>
2015-09-16 10:02:07 -0700

    remove paypal_recurring.rb as no longer needed

--- config/initializers/paypal_recurring.rb
+++
@@ -1,6 +0,0 @@
-PayPal::Recurring.configure do |config|
-  config.sandbox = (ENV['PAYPAL_SANDBOX']=='true')
-  config.username = ENV['PAYPAL_USERNAME']
-  config.password = ENV['PAYPAL_PASSWORD']
-  config.signature = ENV['PAYPAL_SIGNATURE']
-end
