87fcbdc4ec4cf667bcaba42b193cedcead37d961
Duane M. Moody <dmmoody@gmail.com>
2015-09-21 06:34:48 -0700

    add planting js and dynamic select box

---
+++ app/assets/javascripts/planting.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+  varietals = $('#planting_varietal_id').html();
+  $('#planting_crop_id').change(function() {
+    crops = $('#planting_crop_id :selected').text();
+    options = $(varietals).filter("optgroup[label=" + crops + "]").html();
+    if (options) {
+      $('#planting_varietal_id').html(options);
+    } else {
+      $('#planting_varietal_id').empty();
+    }
+  })
+});
