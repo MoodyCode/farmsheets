25a1b183d59df9b5277376a60e746b3f8662c8bb
Duane M. Moody <dmmoody@gmail.com>
2015-10-16 11:31:57 -0700

    fix subscription status

--- app/controllers/stripe_controller.rb
+++ app/controllers/stripe_controller.rb
@@ -5,7 +5,7 @@ class StripeController < ApplicationController
     event = Stripe::Event.retrieve(params[:id])
     stripe_event = event.data.object
     customer = Stripe::Customer.retrieve(stripe_event.customer)
-    stripe_status = customer.subscriptions.data.first.status
+    stripe_status = customer.subscriptions.retrieve(customer.subscriptions.data.first.id)
     # stripe_status = event.data.object.status
 
     account = Account.where(stripe_customer_id: customer).first
